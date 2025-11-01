fa9ae5426b59c92098b25cf67887ffc88df4aa1e
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-11-01 12:51:05 +0300

    add layout and about view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'about')
+
+@section('header')
 <h1>О блоге</h1>
+@endsection
+
+@section('content')
 <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
