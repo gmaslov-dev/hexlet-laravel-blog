cadca07df6d9737304a6490f91d4b65e8f15bbba
gmaslov-dev <gmaslov.dev@yandex.ru>
2025-11-10 19:12:38 +0300

    add bootstrap

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hello, World!';
+    return 'Hello, World!!!!';
 })->name('root');
 
 Route::get('about', function() {
